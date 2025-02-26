5110a222159665dd8c89c6401640ec7c1f6cbde9
noahrandel <Nhrandel89@gmail.com>
2025-02-25 16:35:44 -0800

    done!

--- src/main/java/cs113/listGame/enemies/EnemyArrayList.java
+++ src/main/java/cs113/listGame/enemies/EnemyArrayList.java
@@ -1,19 +1,36 @@
 package cs113.listGame.enemies;
 
 import cs113.listGame.behaviors.Behavior;
+import cs113.listGame.behaviors.MoveToPoint;
+import cs113.listGame.behaviors.Waiting;
 import cs113.listGame.gamecore.GameObject;
+import javafx.geometry.Point2D;
 import javafx.scene.image.ImageView;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class EnemyArrayList extends EnemyObject {
+   ArrayList<Behavior> behaviors = new ArrayList<>();
+   Random rand = new Random();
+
     public EnemyArrayList(ImageView imageView) { super(imageView); }
 
     @Override
     public void seedBehaviors() {
-
+        Point2D dest = new Point2D(rand.nextDouble(500), rand.nextDouble(400));
+        behaviors.add( new Waiting(this, 3000) );
+        behaviors.add(new MoveToPoint(this, 4000, dest));
     }
 
     @Override
     public Behavior getNextBehavior() {
-        return null;
+        if (behaviors.isEmpty()) {
+            return null;
+        }
+
+        int index = rand.nextInt(behaviors.size());
+
+        return behaviors.remove(index);
     }
 }
--- src/main/java/cs113/listGame/resources/ResourceArrayList.java
+++ src/main/java/cs113/listGame/resources/ResourceArrayList.java
@@ -2,24 +2,48 @@ package cs113.listGame.resources;
 
 import cs113.listGame.gamecore.GameObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResourceArrayList implements ResourceList {
+
+    public ArrayList<ResourceObject> resourceList;
+
+    public ResourceArrayList() {
+        resourceList = new ArrayList<>();
+    }
+
     @Override
     public void add(ResourceObject resource) {
-
+        if (resource != null) {
+            resourceList.add(resource);
+        }
     }
 
     @Override
     public void remove(ResourceObject resource) {
-
+        if (resource != null) {
+            resourceList.remove(resource);
+        }
     }
 
     @Override
     public void truncate(ResourceObject resource) {
+        for (int i = 0; i < resourceList.size(); i++) {
+            if(resourceList.get(i).equals(resource)){
+                resourceList.remove(i);
+                break;
+            }
+        }
 
     }
 
     @Override
     public void follow(GameObject leader) {
-
+        for (ResourceObject resource : resourceList) {
+            if (resource != null) {
+                resource.moveTowards(leader.getEchoCenter());
+                leader = resource; }
+        }
     }
 }
